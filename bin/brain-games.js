62dcb53a95061f711789c38a86346058eb93bd04
Shima <dpshedr@mail.ru>
2025-06-09 17:39:00 +0300

    qwrerwe

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
+import greetUser from '../src/cli.js'
 
-import { greetUser  } from '../src/cli.js';
-greetUser();
-
-console.log("Welcome to the Brain Games!");
+greetUser()
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,10 @@
-import readlineSync from 'readline-sync';
-export const greetUser  = () => {
-    
-const userName = readlineSync.question('May I have your name? ');
+import readlineSync from 'readline-sync'
 
-console.log(`Hello, ${userName}!`);
-};
+const greetUser = () => {
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log('Hello, ' + userName + '!')
+  return userName
+}
+
+export default greetUser
